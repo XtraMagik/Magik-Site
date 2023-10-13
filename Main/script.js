74b5d2675ea93814f9767e73ee88772f9c2c9695
XtraMagik <138348122+XtraMagik@users.noreply.github.com>
2023-10-13 07:16:04 -0700

    Update File Script (Update.File.JS_)
    Update File Script (Update.File.JS_)

--- Main/script.js
+++ Main/script.js
@@ -1,11 +1,10 @@
 window.onload = function(){
   Particles.init({
     selector: '.background',
-    maxParticles: 150,
-    connectParticles: 'true',
-    speed:2,
-    minDistance: 140,
-    sizeVariation: 4,
-    color: '#ffffff'
+    maxParticles: 100,
+    speed:0.5,
+    minDistance: 150,
+    sizeVariation: 10,
+    color: '#f705e3'
   });
 }
